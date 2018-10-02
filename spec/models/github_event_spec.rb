44a70eb3e329618bd28aa466bbf7f6e986c8e930
Scott Albertson <ascottalbertson@gmail.com>
2018-10-01 22:39:21 -0700

    Code style updates

--- spec/models/github_event_spec.rb
+++ spec/models/github_event_spec.rb
@@ -109,7 +109,9 @@ RSpec.describe GitHubEvent do
           owner: owner,
           installation_id: nil,
         )
-        body = JSON.parse(read_fixture("github_installation_repositories_added.json"))
+        body = JSON.parse(
+          read_fixture("github_installation_repositories_added.json")
+        )
         event = described_class.new(
           type: GitHubEvent::INSTALLATION_REPOSITORIES,
           body: body,
@@ -132,7 +134,9 @@ RSpec.describe GitHubEvent do
           owner: owner,
           installation_id: 2,
         )
-        body = JSON.parse(read_fixture("github_installation_repositories_removed.json"))
+        body = JSON.parse(
+          read_fixture("github_installation_repositories_removed.json")
+        )
         event = described_class.new(
           type: GitHubEvent::INSTALLATION_REPOSITORIES,
           body: body,
@@ -154,7 +158,9 @@ RSpec.describe GitHubEvent do
           owner: owner,
           installation_id: 2,
         )
-        body = JSON.parse(read_fixture("github_installation_repositories_removed.json"))
+        body = JSON.parse(
+          read_fixture("github_installation_repositories_removed.json"),
+        )
         event = described_class.new(
           type: GitHubEvent::INSTALLATION_REPOSITORIES,
           body: body,
